43218a82c6c0c849bb40fd74261f6869cb88476d
Callum Neilson <2770691n@student.gla.ac.uk>
2024-02-07 14:12:58 +0000

    Chptr 8 testscompl

--- rango/views.py
+++ rango/views.py
@@ -9,11 +9,6 @@ from django.urls import reverse
 
 
 def index(request):
-    # Query the database for a list of ALL categories currently stored.
-    # Order the categories by the number of likes in descending order.
-    # Retrieve the top 5 only -- or all if less than 5.
-    # Place the list in our context_dict dictionary (with our boldmessage!) 
-    # that will be passed to the template engine.
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
 
@@ -22,7 +17,6 @@ def index(request):
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
 
-    # Render the response and send it back!
     response = render(request, 'rango/index.html', context=context_dict)
     return response
 
